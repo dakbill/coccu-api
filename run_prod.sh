26d4039cb0e323bdde0760f6c7cf7685c4eca316
Kofi Dwomoh-Ababio <kofi.dwomoh-ababio@minex360.com>
2022-09-15 23:14:39 +0000

    en: migrates to postgres

--- run_prod.sh
+++ run_prod.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 kill -9 $(lsof -t -i:18080) && \
-export $(cat .env | xargs) ./mvnw spring-boot:run -Dmaven.test.skip=true -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev" &
+export $(cat .env | xargs) && \
+./mvnw spring-boot:run -Dmaven.test.skip=true -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev" &
