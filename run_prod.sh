3239c4d11c0dcac83c39c39fca79e8c58456de19
Kofi Dwomoh-Ababio <kofi.dwomoh-ababio@minex360.com>
2022-09-15 22:59:15 +0000

    en: migrates to postgres

--- run_prod.sh
+++ run_prod.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-#JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home \
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64 && \
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
 ./mvnw spring-boot:run \
 -Dmaven.test.skip=true \
 -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev" &
