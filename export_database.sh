2595558e3deb9a70219a7d41e70e0798c39abaac
Kofi Dwomoh-Ababio <kofi.dwomoh-ababio@minex360.com>
2022-09-16 11:35:10 +0000

    en: updates

---
+++ export_database.sh
@@ -0,0 +1,1 @@
+pg_dump -U ${DB_USERNAME} -h ${DB_HOST} ${DB_NAME} >> coccu.sql
--- run_prod.sh
+++ run_prod.sh
@@ -2,3 +2,4 @@
 kill -9 $(lsof -t -i:18080)
 export $(cat .env | xargs)
 ./mvnw spring-boot:run -Dmaven.test.skip=true -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev" &
+pg_dump -U username -h localhost databasename >> sqlfile.sql
