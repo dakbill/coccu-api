9962d030dfa4ac693e79b4abdbfaa6f4a7f0b54f
Kofi Dwomoh-Ababio <kofi.dwomoh-ababio@minex360.com>
2023-01-07 12:24:16 +0000

    en: adds guuarantor account when showig member details

--- src/main/resources/queries/normalise.sql
+++ src/main/resources/queries/normalise.sql
@@ -1,4 +1,3 @@
 UPDATE "transaction" SET account_id='LOAN-200-1' WHERE account_id='LOAN-200-2' AND amount=100 AND created_date::date='2015-08-15' AND type='LOAN_REPAYMENT';
-
 UPDATE "transaction" SET account_id='LOAN-406-1' WHERE account_id='LOAN-406-2' AND amount=300 AND created_date::date='2015-02-08' AND type='LOAN';
 DELETE FROM "account"  WHERE id='LOAN-406-2';
