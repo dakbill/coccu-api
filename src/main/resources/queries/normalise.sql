4f6b28b8b0403f4fbc65137f28c3380f5abe7b04
Kofi Dwomoh-Ababio <kofi.dwomoh-ababio@minex360.com>
2023-01-10 01:07:30 +0000

    en: adds new transactions

--- src/main/resources/queries/normalise.sql
+++ src/main/resources/queries/normalise.sql
@@ -1,3 +1,5 @@
+drop database coccu;
+create database coccu;
 \c coccu
 UPDATE "transaction" SET account_id='LOAN-200-1' WHERE account_id='LOAN-200-2' AND amount=100 AND created_date::date='2015-08-15' AND type='LOAN_REPAYMENT';
 UPDATE "transaction" SET account_id='LOAN-406-1' WHERE account_id='LOAN-406-2' AND amount=300 AND created_date::date='2015-02-08' AND type='LOAN';
