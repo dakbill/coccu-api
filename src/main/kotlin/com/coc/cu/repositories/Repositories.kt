680135f791f990353c42e5a20764fa067ef95c6e
Kofi Dwomoh-Ababio <kofi.dwomoh-ababio@minex360.com>
2023-01-23 11:19:41 +0000

    en: allows filter by multiple transactionsn

--- src/main/kotlin/com/coc/cu/repositories/Repositories.kt
+++ src/main/kotlin/com/coc/cu/repositories/Repositories.kt
@@ -24,13 +24,13 @@ interface AccountTransactionsRepository: JpaRepository<Transaction, Long> {
                 "  ( (LENGTH(?1)=0) OR ( SELECT LOWER(name) LIKE '%' || ?1 || '%' FROM member WHERE id=(SELECT member_id FROM ACCOUNT WHERE id=TRANSACTION.account_id LIMIT 1) )   )   " +
                 " AND ( (?2=0) OR (account_id IN (SELECT id FROM ACCOUNT WHERE member_id=?2)) )   " +
                 " AND  ( (LENGTH(?3)=0) OR (?3 = \"account_id\" ) )   " +
-                " AND  ( ('EMPTY' = ANY(?4)) OR ( \"type\" = ANY(?4) ) )   " +
+                " AND  ( ('EMPTY' IN ?4) OR ( \"type\" IN ?4 ) )   " +
                 " AND  ( created_date BETWEEN CAST(?5 AS DATE) AND CAST(?6 AS DATE) )   ",
         countQuery = "SELECT COUNT(TRANSACTION.id) FROM TRANSACTION WHERE " +
                 "  ( (LENGTH(?1)=0) OR ( SELECT LOWER(name) LIKE '%' || ?1 || '%' FROM member WHERE id=(SELECT member_id FROM ACCOUNT WHERE id=TRANSACTION.account_id LIMIT 1) )   )   " +
                 " AND ( (?2=0) OR (account_id IN (SELECT id FROM ACCOUNT WHERE member_id=?2)) )   " +
                 " AND  ( (LENGTH(?3)=0) OR (?3 = \"account_id\" ) )   " +
-                " AND  ( ('EMPTY' = ANY(?4)) OR ( \"type\" = ANY(?4) ) )   " +
+                " AND  ( ('EMPTY' IN ?4) OR ( \"type\" IN ?4 ) )   " +
                 " AND  ( created_date BETWEEN CAST(?5 AS DATE) AND CAST(?6 AS DATE) )   ",
         nativeQuery = true
     )
