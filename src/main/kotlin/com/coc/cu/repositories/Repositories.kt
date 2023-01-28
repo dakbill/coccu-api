6481d5a171d2f8bf6003e7814efeec58adaa234f
Kofi Dwomoh-Ababio <kofi.dwomoh-ababio@minex360.com>
2023-01-28 10:42:43 +0000

    wip

--- src/main/kotlin/com/coc/cu/repositories/Repositories.kt
+++ src/main/kotlin/com/coc/cu/repositories/Repositories.kt
@@ -206,11 +206,11 @@ interface MemberAccountRepository : CrudRepository<Account, String> {
         value = "SELECT " +
                 "   account.* FROM account LEFT JOIN member ON(member.id=member_id) " +
                 "WHERE " +
-                "   EXTRACT(year FROM age(NOW(),account.created_date))*12 + EXTRACT(month FROM age(NOW(),account.created_date)) > 12 AND account.type='LOAN' AND account.balance > 0 AND (LOWER(MEMBER.name) LIKE '%' || ?1 || '%' )",
+                "   (EXTRACT(year FROM age(NOW(),account.created_date))*12 + EXTRACT(month FROM age(NOW(),account.created_date))) > 12 AND account.type='LOAN' AND account.balance > 0 AND (LOWER(MEMBER.name) LIKE '%' || ?1 || '%' )",
         countQuery = "SELECT " +
                 "   COUNT(account.id) FROM account LEFT JOIN member ON(member.id=member_id) " +
                 "WHERE " +
-                "   EXTRACT(year FROM age(NOW(),account.created_date))*12 + EXTRACT(month FROM age(NOW(),account.created_date)) > 12 AND account.type='LOAN' AND account.balance > 0 AND (LOWER(MEMBER.name) LIKE '%' || ?1 || '%' )",
+                "   (EXTRACT(year FROM age(NOW(),account.created_date))*12 + EXTRACT(month FROM age(NOW(),account.created_date))) > 12 AND account.type='LOAN' AND account.balance > 0 AND (LOWER(MEMBER.name) LIKE '%' || ?1 || '%' )",
         nativeQuery = true
     )
     fun getOutstandingDebtors(query: String, pageable: Pageable): Page<Account>
@@ -220,11 +220,11 @@ interface MemberAccountRepository : CrudRepository<Account, String> {
         value = "SELECT " +
                 "   account.* FROM account LEFT JOIN member ON(member.id=member_id) " +
                 "WHERE " +
-                "   EXTRACT(year FROM age(NOW(),account.created_date))*12 + EXTRACT(month FROM age(NOW(),account.created_date)) < 12 AND account.type='LOAN' AND account.balance > 0 AND (LOWER(MEMBER.name) LIKE '%' || ?1 || '%' )",
+                "   (EXTRACT(year FROM age(NOW(),account.created_date))*12 + EXTRACT(month FROM age(NOW(),account.created_date))) <= 12 AND account.type='LOAN' AND account.balance > 0 AND (LOWER(MEMBER.name) LIKE '%' || ?1 || '%' )",
         countQuery = "SELECT " +
                 "   COUNT(account.id) FROM account LEFT JOIN member ON(member.id=member_id) " +
                 "WHERE " +
-                "   EXTRACT(year FROM age(NOW(),account.created_date))*12 + EXTRACT(month FROM age(NOW(),account.created_date)) < 12 AND account.type='LOAN' AND account.balance > 0 AND (LOWER(MEMBER.name) LIKE '%' || ?1 || '%' )",
+                "   (EXTRACT(year FROM age(NOW(),account.created_date))*12 + EXTRACT(month FROM age(NOW(),account.created_date))) <= 12 AND account.type='LOAN' AND account.balance > 0 AND (LOWER(MEMBER.name) LIKE '%' || ?1 || '%' )",
         nativeQuery = true
     )
     fun getDefaultingDebtors(query: String, pageable: Pageable): Page<Account>
