7316ec64ee638fc264ca0442625d2a3c13779486
Kofi Dwomoh-Ababio <kofi.dwomoh-ababio@minex360.com>
2024-01-20 00:06:31 +0000

    en: update trasacntions

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export DB_HOST=127.0.0.1 && export DB_PORT=6005 && export DB_NAME=coccu && \
 export DB_USERNAME=smartsapp && export DB_PASSWORD=geGlouvatlanifCiWav5 && \
-JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home \
+JAVA_HOME=/Users/minexrnd/Library/Java/JavaVirtualMachines/openjdk-21.0.2/Contents/Home \
 ./mvnw clean spring-boot:run \
 -Dmaven.test.skip=true \
 -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
