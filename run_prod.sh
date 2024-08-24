e3460c67e0e97ecb8ce53a6a1bc9d709f2699638
Kofi Dwomoh <kofi.dwomoh-ababio@smartsapp.com>
2024-08-24 12:02:39 +0000

    fix: reverts nginx cors settings

--- run_prod.sh
+++ run_prod.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
-kill -9 $(lsof -t -i:18080)
-export $(cat .env | xargs)
-./mvnw spring-boot:run -Dmaven.test.skip=true -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev" &
+##!/bin/sh
+#kill -9 $(lsof -t -i:18080)
+#export $(cat .env | xargs)
+#./mvnw spring-boot:run -Dmaven.test.skip=true -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev" &
+
+./mvnw clean package && docker-compose -f ~/docker-compose.yml up -d coccu-api
