69c50c1fb8f6e0f9fcba02540714abe95f752bae
Kofi Dwomoh <kofi.dwomoh-ababio@smartsapp.com>
2024-10-17 14:35:50 +0000

    updates

--- run_prod.sh
+++ run_prod.sh
@@ -2,5 +2,5 @@
 #kill -9 $(lsof -t -i:18080)
 #export $(cat .env | xargs)
 #./mvnw spring-boot:run -Dmaven.test.skip=true -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev" &
-
+git pull
 ./mvnw clean package -Dmaven.test.skip=true -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=dev" && docker compose up -d --build --force-recreate coccu-api
